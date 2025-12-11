0ce73fba5e0455955147a765c9e20441d848345e
Toricane <prjwl028@gmail.com>
2025-12-11 15:50:28 -0800

    day05

--- day05.cpp
+++ day05.cpp
@@ -1,5 +1,5 @@
 // Day 5
-// Name:
+// Name: Prajwal Prashanth
 
 #include "utils.h"
 
@@ -7,11 +7,17 @@
 #include <ostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
 
 using advent::read_lines;
 using std::cout;
+using std::max;
 using std::ostream;
+using std::sort, std::binary_search;
 using std::string;
+using std::stringstream;
 using std::vector;
 
 // load the raw puzzle input from the default data file
@@ -24,6 +30,17 @@ vector<string> parse_input()
    return read_lines();
 }
 
+struct Range
+{
+   long long min;
+   long long max;
+
+   bool operator<(const Range &range2) const
+   {
+      return min < range2.min;
+   }
+};
+
 // compute the answer for Day 5 Part 1 based on the parsed input
 // preconditions:
 //    lines contains the puzzle input returned from parse_input()
@@ -31,7 +48,66 @@ vector<string> parse_input()
 //    returns the computed Part 1 result as a 64-bit integer
 long long part1(const vector<string> &lines)
 {
-   return 0;
+   vector<Range> ranges;
+   vector<long long> ingredients;
+   bool blank = false;
+
+   for (const string line : lines)
+   {
+      if (line.size() == 0)
+      {
+         blank = true;
+      }
+      else if (!blank)
+      {
+         Range range;
+         stringstream ss(line);
+         char temp;
+
+         ss >> range.min >> temp >> range.max;
+         ranges.push_back(range);
+      }
+      else
+      {
+         ingredients.push_back(stoll(line));
+      }
+   }
+
+   sort(ranges.begin(), ranges.end());
+
+   vector<Range> mergedRanges;
+   mergedRanges.push_back(ranges[0]);
+
+   for (unsigned i = 1; i < ranges.size(); i++)
+   {
+      Range &current = ranges[i];
+      Range &last = mergedRanges.back();
+
+      if (current.min <= last.max + 1)
+      {
+         last.max = max(last.max, current.max);
+      }
+      else
+      {
+         mergedRanges.push_back(current);
+      }
+   }
+
+   long long fresh = 0;
+
+   for (const long long i : ingredients)
+   {
+      for (const Range range : mergedRanges)
+      {
+         if (range.min <= i && i <= range.max)
+         {
+            fresh++;
+            break;
+         }
+      }
+   }
+
+   return fresh;
 }
 
 // compute the answer for Day 5 Part 2 based on the parsed input
@@ -41,7 +117,53 @@ long long part1(const vector<string> &lines)
 //    returns the computed Part 2 result as a 64-bit integer
 long long part2(const vector<string> &lines)
 {
-   return 0;
+   vector<Range> ranges;
+
+   for (const string line : lines)
+   {
+      if (line.size() == 0)
+      {
+         break;
+      }
+      else
+      {
+         Range range;
+         stringstream ss(line);
+         char temp;
+
+         ss >> range.min >> temp >> range.max;
+         ranges.push_back(range);
+      }
+   }
+
+   sort(ranges.begin(), ranges.end());
+
+   vector<Range> mergedRanges;
+   mergedRanges.push_back(ranges[0]);
+
+   for (unsigned i = 1; i < ranges.size(); i++)
+   {
+      Range &current = ranges[i];
+      Range &last = mergedRanges.back();
+
+      if (current.min <= last.max + 1)
+      {
+         last.max = max(last.max, current.max);
+      }
+      else
+      {
+         mergedRanges.push_back(current);
+      }
+   }
+
+   long long fresh = 0;
+
+   for (const Range range : mergedRanges)
+   {
+      fresh += range.max - range.min + 1;
+   }
+
+   return fresh;
 }
 
 // orchestrate input parsing and rendering of both part results
